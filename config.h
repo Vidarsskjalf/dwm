25158d642dae7a0fb83ee62e2cf873d0651eecf6
Jerrod Havig <secure.havig@gmail.com>
2024-06-29 09:33:03 -0700

    Added screenshot capability and changed statusbar color

--- config.h
+++ config.h
@@ -9,15 +9,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "GeistMonoNerdFont:size=14", "NotoColorEmoji:pixelsize=14:antialias=true:autohint=true" };
 static const char dmenufont[]       = "GeistMonoNerdFont:size=14";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#191724"; // unfocused tab color, and top right bar
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#908caa"; //color of titlepart of the panel 4C566A
+static const char col_statusbar[]   = "#26233A"; // focused tab color
+static const char col_statusbar_text[]   = "#E0DEF4"; // statusbar text
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_statusbar_text, col_statusbar,  col_statusbar  },
 };
 
 /* tagging */
@@ -60,16 +60,16 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_statusbar, "-sf", col_statusbar_text, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 /* Volume control */
 static const char *upvol[]   = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL };
 static const char *downvol[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL };
 static const char *mutevol[] = { "pactl", "set-sink-mute", "@DEFAULT_SINK", "toggle", NULL };
-/* Brightness control */
-static const char *brightnessup[] = { "brightnessctl", "set", "10%+", NULL };
-static const char *brightnessdown[] = { "brightnessctl", "set", "10%-", NULL };
+
+/* Screenshot */
+static const char *screenshotcmd[]  = { "flameshot", "gui", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -83,8 +83,9 @@ static const Key keys[] = {
 	{ 0,							XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
 	{ 0,							XF86XK_AudioRaiseVolume, spawn, {.v = upvol } },
 	{ 0,							XF86XK_AudioMute,        spawn, {.v = mutevol } },
-	{ 0,							XF86XK_MonBrightnessUp,	spawn, {.v = brightnessup } },
-	{ 0,							XF86XK_MonBrightnessDown, spawn, {.v = brightnessdown } },
+	{ 0,							XF86XK_MonBrightnessUp,  spawn, SHCMD("~/build/suckless/dwm/scripts/adjust_brightness.sh up") },
+	{ 0,							XF86XK_MonBrightnessDown,spawn, SHCMD("~/build/suckless/dwm/scripts/adjust_brightness.sh down") },
+	{ 0,							XK_Print,  spawn,		   {.v = screenshotcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
