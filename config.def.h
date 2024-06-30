4acb4efd6a2c2bf0c466eab4427d2206721058db
Jerrod Havig <secure.havig@gmail.com>
2024-06-29 18:29:23 -0700

    Patched attachBelow and systray

--- config.def.h
+++ config.def.h
@@ -5,19 +5,24 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gap size between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "GeistMonoNerdFont:size=14", "NotoColorEmoji:pixelsize=14:antialias=true:autohint=true" };
 static const char dmenufont[]       = "GeistMonoNerdFont:size=14";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#191724"; // unfocused tab color, and top right bar
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#4C566A"; //color of titlepart of the panel
+static const char col_statusbar[]   = "#26233A"; // focused tab color
+static const char col_statusbar_text[]   = "#E0DEF4"; // statusbar text
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_statusbar_text, col_statusbar,  col_statusbar  },
 };
 
 /* tagging */
@@ -60,28 +65,32 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_statusbar, "-sf", col_statusbar_text, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 /* Volume control */
-static const char *upvol[]   = { "/home/cursed/build/suckless/dwm/scripts/adjust_volume.sh", "up", NULL };
-static const char *downvol[] = { "/home/cursed/build/suckless/dwm/scripts/adjust_volume.sh", "down", NULL };
-static const char *mutevol[] = { "/home/cursed/build/suckless/dwm/scripts/adjust_volume.sh", "mute", NULL };
-/* Brightness control */
-static const char *brightnessup[] = { "brightnessctl", "set", "10%+", NULL };
-static const char *brightnessdown[] = { "brightnessctl", "set", "10%-", NULL };
+static const char *upvol[]   = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL };
+static const char *downvol[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL };
+static const char *mutevol[] = { "pactl", "set-sink-mute", "@DEFAULT_SINK", "toggle", NULL };
+
+/* Screenshot */
+static const char *screenshotcmd[]  = { "flameshot", "gui", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,						XK_e,      spawn,          SHCMD( "thunar" )},
+	{ MODKEY,						XK_r,      spawn,          SHCMD( "remmina" )},
+	{ MODKEY,						XK_s,      spawn,          SHCMD( "signal-desktop" )},
+	{ MODKEY,						XK_v,      spawn,          SHCMD( "virt-manager" )},
 	{ MODKEY,						XK_f,      spawn,          SHCMD( "firefox" )},
 	{ 0,							XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
 	{ 0,							XF86XK_AudioRaiseVolume, spawn, {.v = upvol } },
 	{ 0,							XF86XK_AudioMute,        spawn, {.v = mutevol } },
-	{ 0,							XF86XK_MonBrightnessUp,	spawn, {.v = brightnessup } },
-	{ 0,							XF86XK_MonBrightnessDown, spawn, {.v = brightnessdown } },
+	{ 0,							XF86XK_MonBrightnessUp,  spawn, SHCMD("~/build/suckless/dwm/scripts/adjust_brightness.sh up") },
+	{ 0,							XF86XK_MonBrightnessDown,spawn, SHCMD("~/build/suckless/dwm/scripts/adjust_brightness.sh down") },
+	{ 0,							XK_Print,  spawn,		   {.v = screenshotcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
